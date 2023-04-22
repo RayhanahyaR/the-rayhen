8a4c77256274ed93fde2e296fe19f10a9d769bf0
RayhanahyaR <104731980+RayhanahyaR@users.noreply.github.com>
2023-04-22 23:42:24 +0100

    Update Skills.lua

--- src/Features/Skills.lua
+++ src/Features/Skills.lua
@@ -2,13 +2,11 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 -- If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
---- test hi hello hey hi im rayhan hi
-
 --< Main >--
 local Skills = {}
 
 --- @class Skills
+--- test hi hello hey hi im rayhan hi
 --- Description goes here
 function Skills.new(MapLib)
 	local self = setmetatable({}, Skills)
